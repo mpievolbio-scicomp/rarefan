c7aaae2227f4b294ed73dd3372c795906643f02e
Carsten <carsten.fortmann-grote@evolbio.mpg.de>
2020-09-21 11:53:38 +0200

    Adding getopt to test script.

--- run_test.sh
+++ run_test.sh
@@ -1,6 +1,46 @@
-#! /bin/sh
+#! /bin/bash
+set -o errexit -o pipefail -o noclobber -o nounset
 
-TEST_DATA_DIR=data/testdata_small
+! getopt --test > /dev/null
+
+if [[ ${PIPESTATUS[0]} -ne 4 ]]; then
+    echo 'I’m sorry, `getopt --test` failed in this environment.'
+    exit 1
+fi
+
+OPTIONS=d:
+LONGOPTIONS=dataset:
+
+if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
+    # e.g. return value is 1
+    #  then getopt has complained about wrong arguments to stdout
+    exit 2
+fi
+
+d=-
+if [ $# -lt 2 ]; then
+    echo "Usage: run_test.sh [-d | --dataset] DATASET [-h | --help]"
+    exit 1
+fi
+while [[ "$#" -gt 0 ]]; do
+    case $1 in 
+        -d|--dataset)
+            dataset="$2"
+            shift
+            ;;
+        -h|--help)
+            echo "Usage: run_test.sh [-d | --dataset] DATASET [-h | --help]"
+            exit 1
+            ;;
+        *)
+            echo "Unknown parameter passed to $1"
+            exit 3
+            ;;
+    esac
+    shift
+done
+
+TEST_DATA_DIR=data/testdata_${dataset}
 RUN_DATA_DIR=/tmp/testdata
 RUN_OUT_DIR=$RUN_DATA_DIR/out
 
@@ -23,5 +63,4 @@ display $RUN_OUT_DIR/correlations.png &&\
 status=0
 
 # rm -rf $RUN_DATA_DIR
-
 exit ${status}
