646772e37b94f76040ed1d738585916a4f5f6ed7
Carsten <carsten.fortmann-grote@evolbio.mpg.de>
2020-09-29 11:10:04 +0200

    Trying to fix R script.

--- displayREPINsAndRAYTs.R
+++ displayREPINsAndRAYTs.R
@@ -71,13 +71,20 @@ plotREPINs=function(folder,treeFile,type,colorBars,bs,fontsize){
   )
 
   t=read.table(paste0(folder,"/presAbs_",type,".txt"),sep="\t")
-  popSize=data.frame(name=t[,1],
-                     rayts=t[,2],
-                     repins=t[,3],
-                     prop=(t[,5]/t[,3]),
-                     propAll=(t[,3]/t[,6]),
-                     numClus=t[,7],
-                     diffRAYTCluster=t[,7]-t[,2])
+#popSize=data.frame(
+#  )
+#
+
+
+  popSize=data.frame(
+                     name=t$strain,
+                     rayts=t$numRAYTs,
+                     repins=t$numREPINs,
+                     prop=(t$mastersequenceFreq/t$numREPINs),
+                     propAll=(t$numREPINs/t$"allREP\\REPINFreq"),
+                     numClus=t$numberOfRepinClusters,
+                     diffRAYTCluster=(t$numberOfRepinClusters-t$numRAYTs)
+  )
 
 
   tree=read.tree(paste0(folder,"/",treeFile))
@@ -175,8 +182,8 @@ plotCorrelationSingle=function(folder,type,
                                name=F,
                                labelOdd){
     t=read.table(paste0(folder,"/presAbs_",type,".txt"),sep="\t")
-    t$propMaster=t[,5]/t[,3]
-    t$numRepin=t[,3]
+    t$propMaster=(t$mastersequenceFreq/t$numREPINs)
+    t$numRepin=t$numREPINs
     p=ggplot(t,
              aes(x=propMaster,
                  y=numRepin))+
