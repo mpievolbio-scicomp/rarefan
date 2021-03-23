1b49494a4319c1e42b75d2b6193884ad4551def8
Carsten <carsten.fortmann-grote@evolbio.mpg.de>
2021-03-23 08:53:46 +0100

    Disallow remote jconsole connections.

--- app/routes.py
+++ app/routes.py
@@ -164,7 +164,7 @@ def submit():
         java_command = " ".join(['java',
                                      '-Dcom.sun.management.jmxremote',
                                       '-Dcom.sun.management.jmxremote.port=9010',
-                                      '-Dcom.sun.management.jmxremote.local.only=false',
+                                      '-Dcom.sun.management.jmxremote.local.only=true',
                                       '-Dcom.sun.management.jmxremote.authenticate=false',
                                       '-Dcom.sun.management.jmxremote.ssl=false',
                                      '-jar',
