34205877fe11d3ac1136c8512ae7034a2c39400d
Carsten Fortmann-Grote <grotec@.evolbio.mpg.de>
2021-03-23 17:28:47 +0100

    Back button.

--- app/routes.py
+++ app/routes.py
@@ -422,9 +422,10 @@ def files(req_path):
             req_path = req_path[:-1]
         logger.warning("Request dir is %s in (%s).", req_path, os.path.dirname(req_path))
 
-        back_link = url_for('results',
+        back_link = redirect(url_for('results',
                             run_id=os.path.basename(session['tmpdir'])
                             )
+                             )
 
         link_to_parent = True
         # Only insert link to parent dir if not at top level.
