43d3d947c4da6576f762a1a37340275eb7e694b2
Carsten Fortmann-Grote <grotec@evolbio.mpg.de>
2021-04-22 22:15:57 +0200

    Adding script to launch shiny app.

---
+++ run_app.R
@@ -0,0 +1,8 @@
+# TODO: Add comment
+# 
+# Author: Carsten Fortmann-Grote
+###############################################################################
+
+
+library(shiny)
+shiny::runApp("analysis", launch.browser=FALSE, port=7238)
