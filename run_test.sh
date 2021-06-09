522f95eb4d7bac22ef7d1214869edc38cd964831
Carsten Fortmann-Grote <grotec@evolbio.mpg.de>
2021-06-09 14:13:29 +0200

    Add chlororraphis to test data.

--- run_test.sh
+++ run_test.sh
@@ -40,29 +40,46 @@ while [[ "$#" -gt 0 ]]; do
     shift
 done
 
-TEST_DATA_DIR=data/neisseria_${dataset}
+TEST_DATA_DIR=test/data/${dataset}
 RUN_DATA_DIR=/tmp/rarefan_test
 RUN_OUT_DIR=${RUN_DATA_DIR}/out
 
 rsync -ruvL ${TEST_DATA_DIR}/ ${RUN_DATA_DIR}/
-rsync -uv data/yafM_Ecoli.faa ${RUN_DATA_DIR}
+rsync -uv test/data/yafM_Ecoli.faa ${RUN_DATA_DIR}
+rsync -uv test/data/yafM_SBW25.faa ${RUN_DATA_DIR}
 
 status=1
+treename="tmptree"
 java_command="java -Xmx10g -jar REPIN_ecology/REPIN_ecology/build/libs/REPIN_ecology.jar\
     ${RUN_DATA_DIR}\
     ${RUN_OUT_DIR}\
     Nmen_2594.fas\
     55 21\
     ${RUN_DATA_DIR}/yafM_Ecoli.faa\
-    tmptree.nwk\
+    ${treename}.nwk\
     1e-30\
     true\
     "
+
+if [[ $dataset == *"chlororaphis"* ]]; then
+    treename="chlororaphis"
+    RUN_OUT_DIR=${RUN_DATA_DIR}/test_out
+    java_command="java -Xmx10g -jar REPIN_ecology/REPIN_ecology/build/libs/REPIN_ecology.jar\
+        ${RUN_DATA_DIR}/in\
+        ${RUN_OUT_DIR}\
+        chlTAMOak81.fas\
+        55 21\
+        ${RUN_DATA_DIR}/yafM_SBW25.faa\
+        ${treename}.nwk\
+        1e-30\
+        true\
+    "
+fi
 echo $java_command
 
 exec $java_command &&\
-andi ${RUN_DATA_DIR}/*.fas > ${RUN_OUT_DIR}/tmptree.dist &&\
-clustDist ${RUN_OUT_DIR}/tmptree.dist > ${RUN_OUT_DIR}/tmptree.nwk &&\
+andi ${RUN_DATA_DIR}/*.fas > ${RUN_OUT_DIR}/${treename}.dist &&\
+clustDist ${RUN_OUT_DIR}/${treename}.dist > ${RUN_OUT_DIR}/${treename}.nwk &&\
 Rscript ./displayREPINsAndRAYTs.R ${RUN_OUT_DIR} &&\
 display ${RUN_OUT_DIR}/repins.png &&\
 display ${RUN_OUT_DIR}/correlations.png &&\
