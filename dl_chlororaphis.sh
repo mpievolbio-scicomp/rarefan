5070e25d08179d4f6c7d8f85f30a16e8d9e63da7
Carsten <carsten.fortmann-grote@evolbio.mpg.de>
2021-08-05 09:37:18 +0200

    Adjust zenodo url.

--- dl_chlororaphis.sh
+++ dl_chlororaphis.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
-wget https://zenodo.org/record/4043614/files/chlororaphis.zip?download=1 -O /tmp/zenodo.zip && \
+wget https://zenodo.org/record/5139700/files/chlororaphis.zip?download=1 -O /tmp/zenodo.zip && \
 unzip /tmp/zenodo.zip -d test/data/
