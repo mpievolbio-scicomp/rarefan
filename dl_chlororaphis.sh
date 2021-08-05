b9f57e5cd6a8c212ba9bcdacbe55522ac6953965
Carsten <carsten.fortmann-grote@evolbio.mpg.de>
2021-08-05 08:32:38 +0200

    Update download link for P.chlororaphis dataset.

--- dl_chlororaphis.sh
+++ dl_chlororaphis.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
-wget https://zenodo.org/record/4043615/files/chlororaphis.zip?download=1 -O /tmp/zenodo.zip && \
+wget https://zenodo.org/record/4043614/files/chlororaphis.zip?download=1 -O /tmp/zenodo.zip && \
 unzip /tmp/zenodo.zip -d test/data/
