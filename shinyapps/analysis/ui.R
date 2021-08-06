933303227fdf22c8402c07b6f14c194c5acbc358
Carsten Fortmann-Grote <grotec@evolbio.mpg.de>
2021-08-06 09:26:25 +0200

    Debugging text output in shiny app.

--- shinyapps/analysis/ui.R
+++ shinyapps/analysis/ui.R
@@ -6,7 +6,6 @@ fluidPage(
 		titlePanel("REPIN and RAYT analysis"),
 		sidebarLayout(
 				sidebarPanel(
-					hr(),
 					selectInput(inputId = 'rayt',
 								label="Select RAYT",
 								choices = list(
@@ -17,15 +16,8 @@ fluidPage(
 										"RAYT 5" = 4,
 										"RAYT 6" = 5
 								),
-								selected = 0),
-					hr(),
-					textOutput("These plots can be reproduced by running the R scripts in the results zip file downloaded from the results page.
-					The syntax is \n
-					\n
-						Rscript run_analysis.R [-r RAYT] [-t TREEFILE] DIR OUTFILE \n
-					\n
-					where DIR is the path to the output directory in the unzipped results dataset and OUTFILE is the file where to save the figure.")
-							),
+								selected = 0)
+						),
 				mainPanel(
 					h4("RAYT tree"),
 					plotOutput(outputId = 'rayt_tree', width="60%", height="400px" ),
@@ -34,7 +26,11 @@ fluidPage(
 					plotOutput(outputId = 'repin_tree', width="60%", height="400px" ),
 					hr(),
 					h4("Correlation"),
-					plotOutput(outputId = 'correlations', width="60%", height="400px" )
+					plotOutput(outputId = 'correlations', width="60%", height="400px" ),
+					textOutput("These plots can be reproduced by running the R scripts in the results zip file downloaded from the results page.
+					The syntax is"),
+					verbatimTextOutput("Rscript run_analysis.R [-r RAYT] [-t TREEFILE] DIR OUTFILE"),
+					textOutput("where DIR is the path to the output directory in the unzipped results dataset and OUTFILE is the file where to save the figure.")
 				)
 		)
 )
