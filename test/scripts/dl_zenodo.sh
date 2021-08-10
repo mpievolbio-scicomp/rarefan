43b21228aaa4e597109c594f28139ecd8f05cc2c
Carsten Fortmann-Grote <grotec@.evolbio.mpg.de>
2021-08-10 22:33:46 +0200

    Reducing verbosity in wget.

--- test/scripts/dl_zenodo.sh
+++ test/scripts/dl_zenodo.sh
@@ -3,17 +3,17 @@
 DATASET="$1"
 
 chlororaphis() {
-	wget https://zenodo.org/record/5139700/files/chlororaphis.zip?download=1 -O /tmp/zenodo.zip && \
+	wget -nv https://zenodo.org/record/5139700/files/chlororaphis.zip?download=1 -O /tmp/zenodo.zip && \
 	unzip -q /tmp/zenodo.zip -d ../data/
 }
 
 dokdonia() {
-	wget https://zenodo.org/record/5139710/files/dokdonia.zip?download=1 -O /tmp/zenodo.zip && \
+	wget -nv https://zenodo.org/record/5139710/files/dokdonia.zip?download=1 -O /tmp/zenodo.zip && \
 	unzip -q /tmp/zenodo.zip -d ../data/
 }
 
 neisseria() {
-	wget https://zenodo.org/record/5139705/files/neisseria.zip?download=1 -O /tmp/zenodo.zip && \
+	wget -nv https://zenodo.org/record/5139705/files/neisseria.zip?download=1 -O /tmp/zenodo.zip && \
 	unzip -q /tmp/zenodo.zip -d ../data/
 }
  
