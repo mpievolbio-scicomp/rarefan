45d3ce61d182df0cd696a57a81f0a40fe91bf520
Carsten Fortmann-Grote <grotec@.evolbio.mpg.de>
2021-08-10 19:50:03 +0200

    Debugging dl script on CI.

--- test/scripts/dl_zenodo.sh
+++ test/scripts/dl_zenodo.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-$DATASET=$1
+$DATASET="$1"
 
 chlororaphis() {
 	wget https://zenodo.org/record/5139700/files/chlororaphis.zip?download=1 -O /tmp/zenodo.zip && \
@@ -23,4 +23,4 @@ all() {
 	neisseria
 }
 
-"$1"
+"$DATASET"
