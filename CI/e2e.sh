57b29228883bcd0ab07a150407241a3fd99a1334
Carsten Fortmann-Grote <grotec@evolbio.mpg.de>
2021-08-11 15:38:30 +0200

    Redirect output in e2e docker script.

--- CI/e2e.sh
+++ CI/e2e.sh
@@ -2,7 +2,9 @@
 # End-to-end testing
 # To be run manually inside docker container from top level dir, e.g. docker run mpievolbioscicomp/repinpop_base:latest CI/e2e.sh
 
-git clone https://gitlab.gwdg.de/mpievolbio-scicomp/repinpop -b testing
+OUT=$1
+
+git clone https://gitlab.gwdg.de/mpievolbio-scicomp/repinpop -b testing 2>&1 >> $OUT
 cd repinpop
-CI/build.sh
-CI/test.sh
+CI/build.sh 2>&1 >> $OUT
+CI/test.sh 2>&1 >> $OUT
