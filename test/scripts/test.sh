65095e2f714441b57205480904b11976d5b25ba8
Carsten Fortmann-Grote <grotec@.evolbio.mpg.de>
2021-08-24 08:54:19 +0200

    Update java CLI in tests.

--- test/scripts/test.sh
+++ test/scripts/test.sh
@@ -119,7 +119,13 @@ dataset_vars() {
 
 java_cmd() {
 	dataset_vars
-    javacmd="java -Xmx${mem} -jar ${PROJECT_ROOT_DIR}/REPIN_ecology/REPIN_ecology/build/libs/REPIN_ecology.jar ${RUN_DATA_DIR} ${RUN_OUT_DIR} $ref_strain 55 21 $rayt_faa ${TREENAME}.nwk 1e-30 true"
+    javacmd="java -Xmx${mem} -jar ${PROJECT_ROOT_DIR}/REPIN_ecology/REPIN_ecology/build/libs/REPIN_ecology.jar\
+                                  ${RUN_DATA_DIR}\
+                                  ${RUN_OUT_DIR}\
+                                  ${ref_strain}\
+                                  55 21\
+                                  ${rayt_faa}\
+                                  ${TREENAME}.nwk 1e-30 true 1"
     echo ${javacmd}
 }
 
