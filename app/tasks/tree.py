de9bdb0bd42cf939644a52ec610e2e5c04888fb2
Carsten Fortmann-Grote <grotec@evolbio.mpg.de>
2022-11-08 14:36:49 +0100

    Rendering repin count table.

--- app/tasks/tree.py
+++ app/tasks/tree.py
@@ -92,5 +92,6 @@ def tree_task(run_dir, treefile=None):
 
 def empty_task():
     return {'returncode': 2,
-            'log': "Must provide at least 2 sequences to compute a distance matrix with `andi`. Plotting is disabled."
+            'log': """Must provide at least 2 sequences to compute a distance matrix with `andi`.
+             Plotting is disabled."""
             }
