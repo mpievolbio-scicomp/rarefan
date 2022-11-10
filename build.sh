daffd701f83d8dada9b72d213592c5972e8a7d44
Carsten Fortmann-Grote <grotec@evolbio.mpg.de>
2022-11-10 12:14:20 +0100

    Resurrecting build script.

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
 #! /bin/bash
+cat /proc/meminfo
+conda init bash
+source ~/.bashrc
+conda activate repinpop
 export INSTALL_PREFIX=$(echo $CONDA_PREFIX)
-rm -rf build
 mkdir build
 cd build
 CC=/usr/bin/gcc CXX=/usr/bin/g++ cmake -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX ..
-make repin_ecology
+cd ..
+cd REPIN_ecology/REPIN_ecology 
+gradle build
+cd ../..
